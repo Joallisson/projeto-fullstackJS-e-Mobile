21b75adfac9f2abb7a2e5b5a384b1d5f69c0009e
Joallisson <joallissons@gmail.com>
2021-11-18 22:42:40 -0300

    Correcao da conexao com o mongoDB
    Aula 22: Esse código foi alterado para reparar os erros de conexão com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const moongose = require('mongoose'); //módulo do NodeJS desenvolvido para conectar-se ao MongoDB
 const url = 'mongodb://localhost:27017/todo'; //local onde está o banco de dados
 
-moongose.connect(url, {useNewUrlParse: true}); //fazendo a conexão com o banco de dados
+moongose.connect(url, {useNewUrlParser: true}); //fazendo a conexão com o banco de dados
 
 module.exports = moongose; //Devolver a constamte moongose já conectado com o BD
